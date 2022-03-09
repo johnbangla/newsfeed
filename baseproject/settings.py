2efe6ff84083619677f5867aaa0edb2e53c95585
johnbangla <johnbangla@gmail.com>
2022-03-09 11:33:50 +0600

    Update settings.py

--- baseproject/settings.py
+++ baseproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firstapp',
+    'firstapp',   'widget_tweaks',
     # 'send_mail',
   
 
